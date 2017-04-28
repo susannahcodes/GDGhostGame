f9ebf58fb422c83a6fe4aa8d5e411ad7d4a0c3e7
AmandaSeiken <ams5xy@virginia.edu>
2017-04-28 12:32:38 -0400

    fixed enemy hitbox

--- GD/src/edu/virginia/gameBuilds/Beta.java
+++ GD/src/edu/virginia/gameBuilds/Beta.java
@@ -685,7 +685,7 @@ public class Beta extends Game {
 								}
 							gameOver.setVisible(true);
 							gameOverB = true;
-							System.out.println(room);
+							//System.out.println(room);
 							}
 						}
 					}
@@ -811,17 +811,13 @@ public class Beta extends Game {
 			gameOver.draw(g);
 		}
 		
-		//testing the hitboxes
+		/*testing the hitboxes
 		g.setColor(Color.green);
 		g.drawRect(135, 215, 435, 465);
-		//g.fillRect(135, 215, 435, 465);
-		
+		g.fillRect(135, 215, 435, 465);
 		g.setColor(Color.blue);
-		g.drawRect((int)enemy.getXPos(), (int)enemy.getYPos(), (int)enemy.getHitBox().getWidth(), (int) enemy.getHitBox().getHeight());
-		
-		g.drawRect((int)ghost.getXPos(), (int)ghost.getYPos(), (int)ghost.getHitBox().getWidth(), (int) ghost.getHitBox().getHeight());
-		
-		/*
+		g.drawRect((int)enemy.getHitBox().getX(), (int)enemy.getHitBox().getY(), (int)enemy.getHitBox().getWidth(), (int) enemy.getHitBox().getHeight());
+		g.drawRect((int)ghost.getHitBox().getX(), (int)ghost.getHitBox().getY(), (int)ghost.getHitBox().getWidth(), (int) ghost.getHitBox().getHeight());
 		g.drawRect(620, 215, 435, 465);
 		g.fillRect(620, 215, 435, 465);
 		g.setColor(Color.blue);
